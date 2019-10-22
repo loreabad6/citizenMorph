175986c5baf0346c59ee9a3560a1a80aab00208e
loreabad6 <lore.abad6@gmail.com>
2019-10-22 14:15:14 +0200

    added path, and photo series and overview image attachments

--- DownloadAttachments.py
+++ DownloadAttachments.py
@@ -1,27 +1,46 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
 # Version 1.2
 
-import logging, os, re, datetime
+import logging, os, re, datetime, json
 from IPython.display import display
 from arcgis.gis import GIS
 
 ''' ********************** SCRIPT CONFIGURATION START ********************** '''
+#Define working directory
+path = r'\\shares\sfs_0165_1\ZGIS\RESEARCH\02_PROJECTS\03_Para_26\TCS00047_citizenMorph\04_Work\WP5\Export_files_CM\citizenMorph'
+os.chdir(path)
+
+#Get images from German (DE) or English (EN) survey?
+language = 'EN'
 
 #What is the ID of the Feature Layer you want to download attachments from?
-FeatureLayerId = '092d075f4b3a40f78cf1329b20b0d5e7'
+#Where do you want your attachments stored?
+if language == 'DE':
+    FeatureLayerId = '97a5a105d3b546c78a94e2d85bbce834'
+    SaveAttachmentsTo = '../ScriptDownloads_DE'
+else:
+    FeatureLayerId = 'cf4004ce88cd4061bf91d9f0456ddfec'
+    SaveAttachmentsTo = '../ScriptDownloads_EN'
+
+SaveLogsTo = 'Logging'
 
 #What are your ArcGIS Enterprise/ArcGIS Online credentials? This is case sensitive.
-PortalUserName = ''
-PortalPassword = ''
-PortalUrl = 'https://www.arcgis.com'
-
-#Where do you want your attachments stored?
-SaveAttachmentsTo = 'C:\ScriptDownloads'
-SaveLogsTo = 'Logging'
+with open('credentials.json') as f:
+    data = json.load(f)
+    PortalUserName = data['username']
+    PortalPassword = data['password']
+    
+PortalUrl = 'https://zgis.maps.arcgis.com'
 
 #How do you want your attachments stored? Options are GroupedFolder and IndividualFolder
 #GroupedFolder - Attachments from every feature in each layer is stored in the same folder - attachments are renamed in the format OBJECTID-ATTACHMENTID-OriginalFileName
 #IndividualFolder - A new folder is created for each OBJECTID, and associated attachments are stored in that folder - attachments are renamed in the format ATTACHMENTID-OriginalFileName
-AttachmentStorage = 'GroupedFolder'
+AttachmentStorage = 'IndividualFolder'
 
 #Set to False if ArcGIS Enterprise cert is not valid
 PortalCertVerification = True
@@ -90,7 +109,11 @@ for i in range(len(itemObject.layers)):
 
         #Query to get list of object ids in layer
         featureObjectIds = featureLayer.query(where='1=1', return_ids_only=True)
-
+        featureIds = featureLayer.query(out_fields = ['objectId','globalId'])
+        layer_ids = {}
+        for row in featureIds.to_dict()["features"]:
+            layer_ids[row['attributes']['globalid']] = row['attributes']['objectid']
+        
         #Provide some updates to user...
         logger.info('Time: {}'.format(str(datetime.datetime.now())))
         logger.info('Currently looping through feature attachments in layer {} of {}: storing in folder named "{}"'.format(str(i + 1), str(len(itemObject.layers)), featureLayerName))
@@ -110,6 +133,8 @@ for i in range(len(itemObject.layers)):
                     attachmentName = currentObjectIdAttachments[k]['name']
                     attachmentSize = currentObjectIdAttachments[k]['size']
                     
+                    ## id_dict[attachmentGlobalId] = currentObjectId
+                    
                     if AttachmentStorage == 'IndividualFolder':
                         currentFolder = featureLayerFolder + r'\\' + str(currentObjectId)
                         #Create a folder for attachments
@@ -120,7 +145,7 @@ for i in range(len(itemObject.layers)):
                             logger.info('The size of the current attachment being downloaded is {}MB'.format((attachmentSize/1000000)))
                             currentAttachmentPath = featureLayer.attachments.download(oid=currentObjectId, attachment_id=attachmentId, save_path=currentFolder)
                             #Rename to ensure file name is unique
-                            renameFile(currentAttachmentPath, newAttachmentPath)
+                            renameFile(currentAttachmentPath[0], newAttachmentPath)
                             downloadCounter += 1
                             downloadSizeCounter += attachmentSize
                         else:
@@ -134,7 +159,90 @@ for i in range(len(itemObject.layers)):
                             logger.info('The size of the current attachment being downloaded is {}MB'.format((attachmentSize/1000000)))
                             currentAttachmentPath = featureLayer.attachments.download(oid=currentObjectId, attachment_id=attachmentId, save_path=featureLayerFolder)
                             #Rename to ensure file name is unique
-                            renameFile(currentAttachmentPath, newAttachmentPath)
+                            renameFile(currentAttachmentPath[0], newAttachmentPath)
+                            downloadCounter += 1
+                            downloadSizeCounter += attachmentSize
+                        else:
+                            logger.info('File {} already exists. Not downloading again!'.format(newAttachmentPath))
+                            nonDownloadCounter += 1
+
+                    else:
+                        logger.error('AttachmentStorage option not valid: {}. Valid options are IndividualFolder and GroupedFolder'.format(AttachmentStorage))
+            else:
+                emptyAttachments += 1
+            
+        logger.info('{} of these features do not contain attachments'.format(str(emptyAttachments)))
+    else:
+        logger.info('Layer {} does not have attachments enabled'.format(featureLayer.properties.name))       
+
+
+#Loop through tables in Feature Layer
+for i in range(len(itemObject.tables)):
+    featureLayer = itemObject.tables[i]
+    
+    #Skip layer if attachments are not enabled
+    if featureLayer.properties.hasAttachments == True:
+        #Remove any characters from feature layer name that may cause problems and ensure it's unique...
+        featureLayerName = '{}-{}'.format(str(i), re.sub(r'[^A-Za-z0-9]+', '', featureLayer.properties.name))
+        featureLayerFolder = SaveAttachmentsTo + r'\\' + featureLayerName
+        createFolder(featureLayerFolder)
+        
+        #Query to get list of object ids in layer
+        featureObjectIds = featureLayer.query(where='1=1', return_ids_only=True)
+        featureIds = featureLayer.query(out_fields = ['objectId','parentGlobalId'])
+        table_ids = {}
+        for row in featureIds.to_dict()["features"]:
+            table_ids[row['attributes']['objectid']] = row['attributes']['parentglobalid']
+        
+                #Provide some updates to user...
+        logger.info('Time: {}'.format(str(datetime.datetime.now())))
+        logger.info('Currently looping through feature attachments in layer {} of {}: storing in folder named "{}"'.format(str(i + 1), str(len(itemObject.layers)), featureLayerName))
+        logger.info('There are {} features to iterate in this layer'.format(str(len(featureObjectIds['objectIds']))))
+            
+        #Loop through features in layer
+        emptyAttachments = 0
+        for j in range(len(featureObjectIds['objectIds'])):
+            currentObjectId = featureObjectIds['objectIds'][j]
+            currentParentGlobalId = table_ids[currentObjectId]
+            currentLayerId = layer_ids[currentParentGlobalId]
+            currentObjectIdAttachments = featureLayer.attachments.get_list(oid=currentObjectId)
+
+
+
+            if len(currentObjectIdAttachments) > 0:
+                
+                #Loop through feature attachments and download to appropriate folder
+                for k in range(len(currentObjectIdAttachments)):
+                    attachmentId = currentObjectIdAttachments[k]['id']
+                    attachmentName = currentObjectIdAttachments[k]['name']
+                    attachmentSize = currentObjectIdAttachments[k]['size']
+
+                     
+                    if AttachmentStorage == 'IndividualFolder':
+                        currentFolder = featureLayerFolder + r'\\' + str(currentLayerId)
+                        #Create a folder for attachments
+                        createFolder(currentFolder)
+                        fileName = '{}-{}'.format(attachmentId, attachmentName)
+                        newAttachmentPath = '{}\\{}'.format(currentFolder, fileName)
+                        if not os.path.isfile(newAttachmentPath):
+                            logger.info('The size of the current attachment being downloaded is {}MB'.format((attachmentSize/1000000)))
+                            currentAttachmentPath = featureLayer.attachments.download(oid=currentObjectId, attachment_id=attachmentId, save_path=currentFolder)
+                            #Rename to ensure file name is unique
+                            renameFile(currentAttachmentPath[0], newAttachmentPath)
+                            downloadCounter += 1
+                            downloadSizeCounter += attachmentSize
+                        else:
+                            logger.info('File {} already exists. Not downloading again!'.format(newAttachmentPath))
+                            nonDownloadCounter += 1
+
+                    elif AttachmentStorage == 'GroupedFolder':
+                        fileName = '{}-{}-{}'.format(currentLayerId, attachmentId, attachmentName)
+                        newAttachmentPath = '{}\\{}'.format(featureLayerFolder, fileName)
+                        if not os.path.isfile(newAttachmentPath):
+                            logger.info('The size of the current attachment being downloaded is {}MB'.format((attachmentSize/1000000)))
+                            currentAttachmentPath = featureLayer.attachments.download(oid=currentObjectId, attachment_id=attachmentId, save_path=featureLayerFolder)
+                            #Rename to ensure file name is unique
+                            renameFile(currentAttachmentPath[0], newAttachmentPath)
                             downloadCounter += 1
                             downloadSizeCounter += attachmentSize
                         else:
@@ -152,3 +260,4 @@ for i in range(len(itemObject.layers)):
 
 logger.info('Summary: {} new files have been downloaded totalling {}MB in size'.format(downloadCounter, (downloadSizeCounter/1000000)))
 logger.info('Summary: {} attachments already existed so were not downloaded again'.format(nonDownloadCounter))
+
--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-# Downloading Feature Layer Attachments via the ArcGIS API for Python
+# DOWNLOAD CITIZENMORPH ATTACHMENTS:
+
+## Steps:
+1. Open a Jupyter notebook and load the `DownloadAttachments.ipynb` file
+2. Select if the attachments should be downloaded for the English or German survey
+3. Run the script
+4. (If necessary) when changes are performed to the sript, be sure to download it as a jupyter notebook and python script and save it to the WP5 folder (where the orginial file was loaded from)
+
+
+Already downloaded data will ideally not be downloaded again.
+Right now the attachments are stored in the citizenMorph project folder on WP5. 
+The data is downloaded to the folders `ScriptDownloads_*`, which depends on the language selected.  In them there are two folders:
+- `0-surveyPoint` which contains the overview picture, and
+- `0-loop` which contains the photo series.
+
+Each of these folders contain sub-folders named after the object-id of the collected data. Hence, the first submitted survey has `objectId = 1`, and its overview picture and photo series should be stored in sub-folder 1 for each parent folder. 
+
+
+# ORIGINAL README:
+Downloading Feature Layer Attachments via the ArcGIS API for Python
+
 ## [Copied from GeoNet](https://geonet.esri.com/docs/DOC-10441-downloading-feature-layer-attachments)
 
 I have commonly encountered questions regarding the downloading of attachments (e.g. pictures and documents) from ArcGIS Online Feature Layers. The aim of this is to provide an alternative solution to what's currently on offer.
